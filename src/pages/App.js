48ba7228ed077c26058e9de68be2f9dac688aed6
Javoto <javoto17@gmail.com>
2019-02-01 01:45:41 +0100

    Fix import

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import logo from '../assets/images/logo.svg';
+import '../assets/sass/App.scss';
 
 class App extends Component {
   render() {
