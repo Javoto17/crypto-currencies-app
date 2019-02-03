229af002c7e4e00fdf13dd5fc9c893553b46ce61
Javoto <javoto17@gmail.com>
2019-02-03 23:32:18 +0100

    Updates

---
+++ precache-manifest.fbb227fad0f79a53bdd6afac4747b5fa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a3be6b62b9465471664",
+    "url": "/crypto-currencies-app/static/css/main.ddcbce8a.chunk.css"
+  },
+  {
+    "revision": "2a3be6b62b9465471664",
+    "url": "/crypto-currencies-app/static/js/main.2a3be6b6.chunk.js"
+  },
+  {
+    "revision": "ac64902c15c41a1817cc",
+    "url": "/crypto-currencies-app/static/js/1.ac64902c.chunk.js"
+  },
+  {
+    "revision": "2c86df5a44abede4af39",
+    "url": "/crypto-currencies-app/static/js/runtime~main.2c86df5a.js"
+  },
+  {
+    "revision": "a24a4f274ea7f823eb9688922919965d",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-Regular.a24a4f27.woff2"
+  },
+  {
+    "revision": "48fa4dc96042b459ec496857904b083b",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-Regular.48fa4dc9.woff"
+  },
+  {
+    "revision": "71e7b170aa5d530caecbe5f749efb0d4",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-Regular.71e7b170.eot"
+  },
+  {
+    "revision": "ca5a1a7d55f3bf1c47b3b0b5a9e7edce",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-ExtraboldItalic.ca5a1a7d.woff2"
+  },
+  {
+    "revision": "83b8d24e5d8c4545bf5509c2c04b4376",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-ExtraboldItalic.83b8d24e.eot"
+  },
+  {
+    "revision": "0e5f779972442a21c09f4f8672a90472",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-ExtraboldItalic.0e5f7799.woff"
+  },
+  {
+    "revision": "fd7d35c4990d16416d117284f254c245",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-Regular.fd7d35c4.ttf"
+  },
+  {
+    "revision": "a641df3eaee6bc38deb3b8542326580e",
+    "url": "/crypto-currencies-app/static/media/AvertaDemoPE-ExtraboldItalic.a641df3e.ttf"
+  },
+  {
+    "revision": "efff21c07bad967a4024af6ea14d26d4",
+    "url": "/crypto-currencies-app/static/media/background-home.efff21c0.png"
+  },
+  {
+    "revision": "3483e5673daf01d664dd7f60122f61cd",
+    "url": "/crypto-currencies-app/index.html"
+  }
+];
